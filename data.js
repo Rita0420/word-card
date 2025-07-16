d1a1ad7c24cda69ba5d80d97a63ffb3bca2f4bcd
Rita <qazasd1011@gmail.com>
2025-07-16 16:24:10 +0800

    完成單字卡片

---
+++ data.js
@@ -0,0 +1,82 @@
+let data = [
+    {
+        "id": 1,
+        "word": "algorithm",
+        "pinyin": "/ˈæl.ɡə.rɪ.ðəm/",
+        "chinese": "演算法",
+        "example": "The search engine uses a complex algorithm to rank results.",
+        "example_translation": "搜尋引擎使用複雜的演算法來排名結果。"
+    },
+    {
+        "id": 2,
+        "word": "variable",
+        "pinyin": "/ˈveə.ri.ə.bəl/",
+        "chinese": "變數",
+        "example": "You should declare all your variables at the beginning of the function.",
+        "example_translation": "你應該在函式開始時宣告所有變數。"
+    },
+    {
+        "id": 3,
+        "word": "function",
+        "pinyin": "/ˈfʌŋk.ʃən/",
+        "chinese": "函式、功能",
+        "example": "This function calculates the total price with tax.",
+        "example_translation": "這個函式計算含稅總價。"
+    },
+    {
+        "id": 4,
+        "word": "compile",
+        "pinyin": "/kəmˈpaɪl/",
+        "chinese": "編譯",
+        "example": "You need to compile the code before running it.",
+        "example_translation": "你需要先編譯程式碼才能執行它。"
+    },
+    {
+        "id": 5,
+        "word": "debug",
+        "pinyin": "/ˌdiːˈbʌɡ/",
+        "chinese": "除錯",
+        "example": "He spent the whole afternoon debugging the login system.",
+        "example_translation": "他花了一整個下午在除錯登入系統。"
+    },
+    {
+        "id": 6,
+        "word": "framework",
+        "pinyin": "/ˈfreɪm.wɜːk/",
+        "chinese": "框架",
+        "example": "React is a popular JavaScript framework.",
+        "example_translation": "React 是一個受歡迎的 JavaScript 框架。"
+    },
+    {
+        "id": 7,
+        "word": "library",
+        "pinyin": "/ˈlaɪ.brər.i/",
+        "chinese": "函式庫",
+        "example": "We use the TensorFlow library for machine learning.",
+        "example_translation": "我們使用 TensorFlow 函式庫進行機器學習。"
+    },
+    {
+        "id": 8,
+        "word": "syntax",
+        "pinyin": "/ˈsɪn.tæks/",
+        "chinese": "語法",
+        "example": "Incorrect syntax can cause the program to crash.",
+        "example_translation": "錯誤的語法可能會導致程式崩潰。"
+    },
+    {
+        "id": 9,
+        "word": "parameter",
+        "pinyin": "/pəˈræm.ɪ.tər/",
+        "chinese": "參數",
+        "example": "This function accepts two parameters: width and height.",
+        "example_translation": "這個函式接受兩個參數：寬度與高度。"
+    },
+    {
+        "id": 10,
+        "word": "loop",
+        "pinyin": "/luːp/",
+        "chinese": "迴圈",
+        "example": "The loop runs until the condition is false.",
+        "example_translation": "這個迴圈會持續執行直到條件為假。"
+    }
+]
